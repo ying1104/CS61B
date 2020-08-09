20a52913487ad631d96de8076239d0fac58d98fe
ying1104 <67181806+ying1104@users.noreply.github.com>
2020-08-08 22:17:27 -0700

    finished the simple part.

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -12,6 +12,23 @@ public class OomageTestUtility {
          * and ensure that no bucket has fewer than N / 50
          * Oomages and no bucket has more than N / 2.5 Oomages.
          */
-        return false;
+        int[] buckets = new int[M];
+        for (int i = 0; i < M; i++) {
+            buckets[i] = 0;
+        }
+        int N = oomages.size();
+        double upperLimit = N / 50.0;
+        double lowerLimit = N / 2.5;
+
+        for (Oomage o : oomages) {
+            int bucketNum = (o.hashCode() & 0x7FFFFFFF) % M;
+            buckets[bucketNum] = buckets[bucketNum] + 1;
+        }
+        for (int b : buckets) {
+            if (b < upperLimit || b > lowerLimit) {
+                return false;
+            }
+        }
+        return true;
     }
 }
