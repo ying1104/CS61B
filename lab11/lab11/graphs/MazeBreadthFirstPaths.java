7ed1c826cfbd9950476c4dd87ca495bb3943e8a2
ying1104 <67181806+ying1104@users.noreply.github.com>
2020-08-16 23:08:54 -0700

    first try

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +12,69 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
+    private PriorityQueue<Integer> q;
+
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
+        Comparator<Integer> c = new Comparator<>() {
+            @Override
+            public int compare(Integer i, Integer j) {
+                if (edgeTo[i] == edgeTo[j]) {
+                    return 0;
+                } else if (edgeTo[i] > edgeTo[j]) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        };
+        q = new PriorityQueue<>(c);
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+    private void bfs(int v) {
+        marked[v] = true;
+        announce();
+
+        if (v == t) {
+            targetFound = true;
+        }
+
+        if (targetFound) {
+            return;
+        }
+
+
+        for (int w : maze.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                announce();
+                distTo[w] = distTo[v] + 1;
+                if (targetFound) {
+                    return;
+                }
+                q.add(w);
+            }
+        }
+        int nextMaze = q.peek();
+        q.remove(nextMaze);
+        bfs(nextMaze);
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs(s);
     }
 }
 
